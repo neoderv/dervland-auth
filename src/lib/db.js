9ac66000d3ec31e26c4e059cef929a9a82b3cd55
xenocubium <dravenmonti@gmail.com>
2023-09-24 21:13:15 -0400

    remove db commands (there is a schema)

--- src/lib/db.js
+++ src/lib/db.js
@@ -10,9 +10,6 @@ async function initDb() {
       filename: `${process.cwd()}/db/main.db`,
       driver: sqlite3.Database
     });
-    
-    await db.run('CREATE TABLE IF NOT EXISTS auth (username TEXT, password TEXT, ip TEXT)')
-    await db.run('CREATE TABLE IF NOT EXISTS token (username TEXT, token TEXT, scope TEXT)')
 
     return db;
 }
